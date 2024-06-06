436d8e217c818a589596e729a05627cfcb539d4e
DAnnYilluec <dannyilluec@gmail.com>
2024-06-06 09:14:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <title>Ruleta</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="estilos.css" rel="stylesheet" type="text/css" media="all">
+    <link rel="icon" href="images/demo/logo.png">
+    <script src="https://kit.fontawesome.com/801222a0d1.js" crossorigin="anonymous"></script>
+</head>
+<body id="top">
+<!-- MENU -->
+<div class="bgded" style="background-color: teal">
+    <div class="wrapper row1">
+        <header id="header" class="hoc clear">
+            <div id="logo" class="fl_left">
+                <h1><a href="index.php">ANGIE404_</a></h1>
+            </div>
+            <nav id="mainav" class="fl_right">
+                <ul class="clear">
+                    <li class="active"><a href="Ruleta.php">Ruleta infantil</a></li>
+                    <li><a href="Aleatorio.php">Ruleta Aleatoria</a></li>
+                    <li><a href="Subs.php">Ruleta Subs</a></li>
+                    <li><a href="Seguidores.php">Ver Seguidores</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div class="overlay">
+        <div id="pageintro" class="hoc clear">
+            <article>
+                <h3 class="heading">Extensible angie404_</h3>
+            </article>
+        </div>
+    </div>
+</div>
+<!-- FIN MENU -->
+<div class="wrapper row3">
+    <main class="hoc container clear">
+        <!-- BODY -->
+        <section id="introblocks">
+            <ul class="nospace group btmspace-80">
+                <li class="one_third first">
+                    <figure><a class="imgover" href="Ruleta.php"><img src="roulette.png" alt=""></a>
+                        <figcaption>
+                            <h6 class="heading">Ruleta Infantil</h6>
+                            <div>
+                                <p>¡Pon tu mismo los nombres de máximo 10 personas! ¿A quien le tocará?</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="one_third">
+                        <figure><a class="imgover" href="Aleatorio.php"><img src="roulette.png" alt=""></a>
+                        <figcaption>
+                            <h6 class="heading">Ruleta Aleatoria Total</h6>
+                            <div>
+                                <p>¡Aqui estan todos los seguidores ¿De verdad le vas a dar?!</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="Subs.php"><img src="roulette.png" alt=""></a>
+                        <figcaption>
+                            <h6 class="heading">Ruleta de Subs</h6>
+                            <div>
+                                <p>Al principio, hay que echarle billetes, y si quieres suerte tambien, subscribete cabrón</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
+        </section>
+
+    </main>
+</div>
+</body>
+</html>
